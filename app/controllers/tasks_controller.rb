72fa2c5160dc802c6eb55d1b04b1c6c1bff4f397
Fabien Riou <fabien.riou92@gmail.com>
2020-02-18 15:20:40 +1100

    Raills Task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root to: 'tasks#index'
+
+
+ get 'new', to: 'tasks#new'
+ post 'tasks', to: 'tasks#create'
+
+get 'tasks', to: 'tasks#index'
+ get 'tasks/:id', to: 'tasks#show', as: :task
+
+ delete 'tasks/:id', to: 'tasks#destroy'
+
+ #resources :tasks
 end
